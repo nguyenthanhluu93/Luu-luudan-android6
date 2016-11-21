e7226cc3e666c3bc56237eaf1df88736051c314d
Nguyen Luu <nguyenthanhluu93@gmail.com>
2016-11-21 18:50:57 +0700

    .

--- Lab2_Turn5/app/src/main/java/luunt/lab2_turn5/MainActivity.java
+++ Lab2_Turn5/app/src/main/java/luunt/lab2_turn5/MainActivity.java
@@ -1,14 +1,42 @@
 package luunt.lab2_turn5;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.SeekBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.toString();
+    @BindView(R.id.seek_bar)
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.d(TAG, String.format("%s", i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
+
 
 }
